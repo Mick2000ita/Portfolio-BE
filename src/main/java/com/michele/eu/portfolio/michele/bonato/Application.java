78ea1d1f152c07cf0506193ed2d874b007168e1a
Mick <mick2000ita@gmail.com>
2025-06-30 22:07:04 +0200

    Initial commit of the project, setup spring

---
+++ src/main/java/com/michele/eu/portfolio/michele/bonato/Application.java
@@ -0,0 +1,13 @@
+package com.michele.eu.portfolio.michele.bonato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
